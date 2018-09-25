0c554886c17d0afc66ab112454b18fb7dc842208
subeeshk <subeeshamie@gmail.com>
2018-09-25 16:31:43 +0400

    y

--- pdf.js
+++ pdf.js
@@ -1,3 +1,59 @@
+function fileDownload(dirName, fileURL, fileName,id) {
+
+    window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fsAccess, fsFail);
+
+    function fsAccess(fileSystem) {
+
+        fileSystem.root.getDirectory(dirName, { create: true, exclusive: false }, dirReady, dirFail);
+
+    }
+
+    function fsFail() {
+
+        console.log("Filesystem Access Failed");
+		alert("Filesystem Access Failed")
+
+    }
+
+    function dirFail() {
+
+        console.log("Directory Access Failed");
+		alert("Directory Access Failed");
+
+    }
+
+    function dirReady(entry) {
+
+        window.appRootDir = entry;
+
+        console.log(JSON.stringify(window.appRootDir));
+
+        var fileTransfer = new FileTransfer();
+
+        fileTransfer.download(
+            fileURL,
+            window.appRootDir.nativeURL + fileName,
+            function (theFile) {
+				
+				var furl= theFile.toURL();
+				alert("download complete: " + theFile.toURL());
+       
+				pdfcomplete(id,furl);
+            },
+            function (error) {
+				alert(JSON.stringify(error));
+                console.log(JSON.stringify(error));
+				pdferror(id);
+            }
+        );
+
+    }
+
+}
+
+
 function PdfAttachment(content,id){
 	 $$("#pdf1").attr("src", "Images/PdfL.gif");
 	 
@@ -15,7 +71,6 @@ function pdferror(id){
 }
 function pdfcomplete(id,toURL){
 	$$("#pdf1").attr("src", "Images/pdfC.png");
-		alert(JSON.stringify(toURL));
+		alert(toURL);
 	
 }
-
