3f30774c2184d554f46ff8c8f576f1ac3128067d
subeeshk <subeeshamie@gmail.com>
2018-09-25 15:22:27 +0400

    dd

--- pdf.js
+++ pdf.js
@@ -1,5 +1,5 @@
 function PdfAttachment(content,id){
-	 $$("#pdf1").attr("src", "PdfL.gif");
+	 $$("#pdf1").attr("src", "Images/PdfL.gif");
 	 
 	 fileDownload('DFMChatbot', 'http://www.inkwelleditorial.com/pdfSample.pdf', 'myfile.pdf');
 	 
