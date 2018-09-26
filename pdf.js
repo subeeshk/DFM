ae9c78d7015790b18c7cee1888fa6f7acf7cf8d8
subeeshk <subeeshamie@gmail.com>
2018-09-26 13:22:01 +0400

    dx

--- pdf.js
+++ pdf.js
@@ -64,7 +64,7 @@ function fileDownload(dirName, fileURL, fileName,id) {
 function PdfAttachment(content,id){
 	 $$("#pdf1").attr("src", "Images/PdfL.gif");
 	 
-	 fileDownload(cordova.file.dataDirectory, content, id+'.pdf',id);
+	 fileDownload("DFMChtbot", content, id+'.pdf',id);
 	 
 	 
 	
